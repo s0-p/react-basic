cbc6d0ab3465c203025f4867554e9c17d6b8247c
s0-p <b00v0429@gmail.com>
2026-06-23 13:16:14 +0900

    feat: Nav 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,28 @@ function MyHeader() {
     </header>
   );
 }
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="">UI/UX 개발</a>
+        </li>
+        <li>
+          <a href="">재사용이 가능한 UI 개발</a>
+        </li>
+        <li>
+          <a href="">애니메이션 구현</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
 function App() {
   return (
     <>
       <MyHeader />
+      <Nav />
     </>
   );
 }
