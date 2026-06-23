74e46cab7fae61d0d4233f18139477ed4da9654d
s0-p <b00v0429@gmail.com>
2026-06-23 13:13:33 +0900

    feat: header 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import './App.css';
 
+function MyHeader() {
+  return (
+    <header>
+      <h1 className="logo">프론트엔드 개발자</h1>
+      <p>기본언어인 html, css, javascript부터 학습합니다.</p>
+    </header>
+  );
+}
 function App() {
-  return <></>;
+  return (
+    <>
+      <MyHeader />
+    </>
+  );
 }
 
 export default App;
