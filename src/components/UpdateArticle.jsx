5ba039ca580e5fdcce13e4b4b1896e350bbe98fe
s0-p <b00v0429@gmail.com>
2026-06-25 09:46:41 +0900

    refactor: form 리팩토링

--- src/components/UpdateArticle.jsx
+++ src/components/UpdateArticle.jsx
@@ -17,40 +17,10 @@ function UpdateArticle({ title, desc, diff, onSubmit }) {
   //   setNewDesc(e.target.value);
   // };
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setContent((prev) => {
-      return {
-        ...prev,
-        [name]: value,
-      };
-    });
-  };
-
   return (
     <>
       <h2>Update Article</h2>
-      <form
-        action=""
-        onSubmit={(e) => {
-          e.preventDefault();
-          onSubmit(content.title, content.desc, content.diff);
-        }}
-      >
-        <div>
-          <label htmlFor="title">title</label>
-          <input type="text" name="title" id="title" value={content.title} onChange={handleChange} />
-        </div>
-        <div>
-          <label htmlFor="desc">desc</label>
-          <textarea name="desc" id="desc" value={content.desc} onChange={handleChange}></textarea>
-        </div>
-        <div>
-          <label htmlFor="diff">diff</label>
-          <input type="number" name="diff" id="diff" value={content.diff} onChange={handleChange} />
-        </div>
-        <button>Submit</button>
-      </form>
+      <ArticleForm initTitle={title} initDesc={desc} initLevel={diff} onSubmit={onSubmit} />
     </>
   );
 }
